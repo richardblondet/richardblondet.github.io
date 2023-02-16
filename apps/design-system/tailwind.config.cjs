c196010018f703732058f6c03f5929ac14288f95
richardblondet <richardblondet@gmail.com>
2023-02-16 00:23:42 -0400

    fresh install of design-system

---
+++ apps/design-system/tailwind.config.cjs
@@ -0,0 +1,20 @@
+const {
+    createGlobPatternsForDependencies,
+} = require('@nxtensions/astro/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: [
+        join(
+            __dirname,
+            'src/**/!(*.stories|*.spec).{astro,html,js,jsx,md,svelte,ts,tsx,vue}'
+        ),
+        ...createGlobPatternsForDependencies(__dirname),
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
