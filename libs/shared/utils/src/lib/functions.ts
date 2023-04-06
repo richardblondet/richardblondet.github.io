dc7b8448222b2b248c691f896abd6d6073bf40b7
richardblondet <richardblondet@gmail.com>
2023-04-06 14:05:06 -0400

    adding a full library for an util function

---
+++ libs/shared/utils/src/lib/functions.ts
@@ -0,0 +1,11 @@
+export function formatIsoDateString(dateString: string) {
+  const date = new Date(dateString);
+  date.setMonth(date.getMonth() + 1); // add 1 month from the date to correct offset
+
+  const monthYearFormatter = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'short',
+  });
+  
+  return monthYearFormatter.format(date);
+}
